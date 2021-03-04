743d68286bce1ccc0827bb7122f5008540119995
Stefan <stefankorbakov@gmail.com>
2021-03-04 21:25:02 +0100

    Implemented basic "search" function

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
     const movieList = document.getElementById('movie-list');
 
     if (movies.lenght === 0) {
@@ -14,7 +14,11 @@ const renderMovies = () => {
     }
     movieList.innerHTML = '';
 
-    movies.forEach((movie) => {
+    const filteredMovies = !filter
+        ? movies
+        : movies.filter((movie) => movie.info.title.includes(filter));
+
+    filteredMovies.forEach((movie) => {
         const movieEl = document.createElement('li');
         let text = movie.info.title + ' - ';
         for (const key in movie.info) {
@@ -25,7 +29,7 @@ const renderMovies = () => {
         movieEl.textContent = text;
         movieList.append(movieEl);
     });
-}
+};
 
 const addMovieHandler = () => {
     const title = document.getElementById('title').value;
@@ -43,13 +47,19 @@ const addMovieHandler = () => {
     const newMovie = {
         info: {
             title,
-            [extraName]: extraValue
+            [extraName]: extraValue,
         },
-        id: Math.random()
-    }
+        id: Math.random(),
+    };
 
     movies.push(newMovie);
     renderMovies();
 };
 
+const searchMovieHandler = () => {
+    const filterTerm = document.getElementById('filter-title').value;
+    renderMovies(filterTerm);
+};
+
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
